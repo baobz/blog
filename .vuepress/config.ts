c6644a8c54529b188d3055a15d968afa402025c2
鲍丙郑 <baobingzheng@example.com>
2022-09-21 19:57:53 +0800

    模板

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,108 +1,44 @@
 import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
-import recoTheme from 'vuepress-theme-reco'
+import { recoTheme } from 'vuepress-theme-reco'
 
 export default defineUserConfig({
-  title: 'vuepress-theme-reco',
-  base: '/blog/',
-  description: 'Just playing around',
+  title: "午后南杂",
+  description: 'Enjoy when you can, and endure when you must.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+  ],
   theme: recoTheme({
     style: '@vuepress-reco/style-default',
-    logo: '/logo.png',
+    logo: '/head.png',
+    // logo: '/hero_black.png',
     author: 'reco_luan',
-    docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
-    docsBranch: 'main',
-    docsDir: 'example',
+    authorAvatar: '/head.png',
+    docsRepo: 'https://github.com/recoluan/recoluan.github.io',
+    docsBranch: 'gh-pages-source',
     lastUpdatedText: '',
-    // series 为原 sidebar
-    series: {
-      '/docs/theme-reco/': [
-        {
-          text: 'module one',
-          children: ['home', 'theme']
-        },
-        {
-          text: 'module two',
-          children: ['api', 'plugin']
-        }
-      ]
-    },
-    navbar:
-    [
-      { text: '首页', link: '/' },
-      { text: '分类', link: '/categories/reco/1/' },
-      { text: '标签', link: '/tags/tag1/1/' },
-      { text: '文档',
+    navbar: [
+      {
+        text: 'Contact',
         children: [
-          { text: 'vuepress-reco', link: '/docs/theme-reco/theme' },
-          { text: 'vuepress-theme-reco', link: '/blogs/other/guide' }
+          { text: 'GitHub', link: 'https://github.com/recoluan' },
         ]
-      },
+      }
     ],
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: 'text',
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: 'font-size: 12px;'
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'title',
-    //       content: 'QQ 群',
-    //     },
-    //     {
-    //       type: 'text',
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: 'font-size: 12px;'
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'title',
-    //       content: 'GitHub',
-    //     },
-    //     {
-    //       type: 'text',
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: 'font-size: 12px;'
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'buttongroup',
-    //       children: [
-    //         {
-    //           text: '打赏',
-    //           link: '/docs/others/donate.html'
-    //         }
-    //       ]
-    //     }
-    //   ],
-    // },
-    // valineConfig 配置与 1.x 一致
-    // valineConfig: {
-    //   appId: 'xxx',
-    //   appKey: 'xxx',
-    //   placeholder: '填写邮箱可以收到回复提醒哦！',
-    //   verify: true, // 验证码服务
-    //   // notify: true,
-    //   recordIP: true,
-    //   // hideComments: true // 隐藏评论
-    // },
+    commentConfig: {
+      type: 'valine',
+      options: {
+        appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
+        appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
+        placeholder: '填写邮箱可以收到回复提醒哦！',
+        verify: true, // 验证码服务
+        // notify: true, //
+        recordIP: true,
+        // hideComments: true
+      },
+    },
+    autoAddCategoryToNavbar: true
   }),
-  // debug: true,
+  lang: 'zh-CN',
 })
